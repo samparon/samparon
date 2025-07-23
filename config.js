04397c14b53e80dbb07e90b37f00d6b373bf0a2c
samparon <guilhermesampaiosantos13@gmail.com>
2025-07-23 14:00:02 -0300

    config.js

--- config.js
+++ config.js
@@ -25,20 +25,20 @@
 //---LINKS DE DEMONSTRAÇÃO ABAIXO: TROQUE PELO SEUS LINKS
 
 // URL of the spreadsheet with menu items (tab: Items)
-const MENU_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTC26EKIY-k7tqv_zVAs91nkoaINfgt5FNO7_-7e_qY3Zm8SG45yAQiDD3GWuDuXCjJvdVqEwpP0omu/pub?gid=1132427241&single=true&output=csv';
+const MENU_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwCsyfX8qG2olzO4XAJjyddufBIdqU1ZLShR3aL72AOAjxCK3MH-cP33jnX5_O7g/pub?gid=1442603174&single=true&output=csv';
 
 // URL of the spreadsheet with menu categories (tab: Categories)
-const CATEGORIES_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTC26EKIY-k7tqv_zVAs91nkoaINfgt5FNO7_-7e_qY3Zm8SG45yAQiDD3GWuDuXCjJvdVqEwpP0omu/pub?gid=271394769&single=true&output=csv';
+const CATEGORIES_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwCsyfX8qG2olzO4XAJjyddufBIdqU1ZLShR3aL72AOAjxCK3MH-cP33jnX5_O7g/pub?gid=225874416&single=true&output=csv';
 
 // URL of the spreadsheet with general settings (tab: Config)
-const CONFIG_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTC26EKIY-k7tqv_zVAs91nkoaINfgt5FNO7_-7e_qY3Zm8SG45yAQiDD3GWuDuXCjJvdVqEwpP0omu/pub?gid=0&single=true&output=csv';
+const CONFIG_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwCsyfX8qG2olzO4XAJjyddufBIdqU1ZLShR3aL72AOAjxCK3MH-cP33jnX5_O7g/pub?gid=672598215&single=true&output=csv';
 
 // URL of the spreadsheet with operating hours (tab: Hours)
-const HOURS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTC26EKIY-k7tqv_zVAs91nkoaINfgt5FNO7_-7e_qY3Zm8SG45yAQiDD3GWuDuXCjJvdVqEwpP0omu/pub?gid=1842804489&single=true&output=csv';
+const HOURS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwCsyfX8qG2olzO4XAJjyddufBIdqU1ZLShR3aL72AOAjxCK3MH-cP33jnX5_O7g/pub?gid=893456414&single=true&output=csv';
 
 // URL of the spreadsheet with neighborhoods and delivery fees (tab: Bairros) **OPCIONAL**
-const NEIGHBORHOODS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTC26EKIY-k7tqv_zVAs91nkoaINfgt5FNO7_-7e_qY3Zm8SG45yAQiDD3GWuDuXCjJvdVqEwpP0omu/pub?gid=1634629476&single=true&output=csv';
+const NEIGHBORHOODS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwCsyfX8qG2olzO4XAJjyddufBIdqU1ZLShR3aL72AOAjxCK3MH-cP33jnX5_O7g/pub?gid=2001677955&single=true&output=csv';
 
 // URL da planilha de cupons de desconto
-const COUPONS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTC26EKIY-k7tqv_zVAs91nkoaINfgt5FNO7_-7e_qY3Zm8SG45yAQiDD3GWuDuXCjJvdVqEwpP0omu/pub?gid=1144201431&single=true&output=csv';
+const COUPONS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwCsyfX8qG2olzO4XAJjyddufBIdqU1ZLShR3aL72AOAjxCK3MH-cP33jnX5_O7g/pub?gid=81782775&single=true&output=csv';
 
